d8624c127fa702348217dab466f36976fbb1d26c
Pawan Kumar <97475613+Pawangithub752000@users.noreply.github.com>
2022-06-18 00:06:38 +0530

    Done

---
+++ src/components/List.js
@@ -0,0 +1,25 @@
+import React from 'react';
+function List(){
+    return (
+        <>
+            <div>
+                <h1>Mobile Operating System</h1>
+                <ul>
+                    <li>Andriod</li>
+                    <li>Blckberry</li>
+                    <li>iPhone</li>
+                    <li>windows Phone</li>
+                </ul>
+                <h1>Mobile Manufacturers</h1>
+                <ul>
+                    <li>Samsung</li>
+                    <li>HTC</li>
+                    <li>Micromax</li>
+                    <li>Apple</li>
+                </ul>
+            </div>
+        </>
+    )
+}
+
+export default List;
